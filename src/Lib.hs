a7747639d904becdda6b76ccb758659f21f1466c
bryanfeliciano <bryanfeliciano892@gmail.com>
2022-06-21 15:59:58 -0400

    started parsing data, just needs to be modified

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,7 @@ import qualified Data.Aeson as JSON
 import qualified Network.HTTP.Client as HTTP
 import Network.HTTP.Client.TLS (newTlsManager)
 import RIO
+import Data.Char (toLower)
 
 data User = User {
     userId :: Int,
@@ -14,6 +15,10 @@ data User = User {
 } deriving (EQ,Show,Generic)
 
 instance ToJSON User where
+    toJSON = JSON.genericToJSON $ JsonOptions "user"
+
+instance FromJSON User where
+    parseJSON = JSON.genericParseJSON $ JsonOptions "user"
 
 getUsersContent :: IO LByteString
 getUsersContent = do
@@ -25,3 +30,10 @@ runMain :: IO()
 runMain = do
     manager <- newTlsManager
     putStrLn "hello,haskell"
+
+JsonOptions :: String -> JSON.JsonOptions
+JsonOptions prefix =
+    let prefixLength =  length prefix
+        lowerCaseFirstCharacter (c:rest) = Char.toLower c : rest
+        lowerCaseFirstCharacter [] = []
+    in JSON.defaultOptions {JSON.fieldLabelModifier = drop prefixLength >>> lowerCaseFirstCharacter}
